13939564a42335ebd2e4459b281429d3f06b9f71
Furqon <ahmad.furqon@gmail.com>
2022-08-30 11:54:43 +0700

    add sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,18 @@
 	import '../app.css';
 
 	import Navbar from '$lib/components/navbar.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte';
+	import Footer from '$lib/components/footer.svelte';
 </script>
 
 <div>
 	<Navbar />
+	<div class="flex overflow-hidden bg-white pt-16">
+		<Sidebar />
+		<div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+			<main><div class="pt-6 px-4"><slot /></div></main>
 
-	<slot />
+			<Footer />
+		</div>
+	</div>
 </div>
--- src/routes/login/+page@blank.svelte
+++ src/routes/login/+page@blank.svelte
@@ -1,77 +1,70 @@
-<!-- component -->
-<!-- This is an example component -->
-<div class="max-w-2xl mx-auto">
-	<div
-		class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700"
-	>
-		<form class="space-y-6" action="#">
-			<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-			<div>
-				<label for="email" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-					>Your email</label
-				>
-				<input
-					type="email"
-					name="email"
-					id="email"
-					class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-					placeholder="name@company.com"
-					required=""
-				/>
-			</div>
-			<div>
-				<label
-					for="password"
-					class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-					>Your password</label
-				>
-				<input
-					type="password"
-					name="password"
-					id="password"
-					placeholder="••••••••"
-					class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-					required=""
-				/>
-			</div>
-			<div class="flex items-start">
-				<div class="flex items-start">
-					<div class="flex items-center h-5">
-						<input
-							id="remember"
-							aria-describedby="remember"
-							type="checkbox"
-							class="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-							required=""
-						/>
-					</div>
-					<div class="text-sm ml-3">
-						<label for="remember" class="font-medium text-gray-900 dark:text-gray-300"
-							>Remember me</label
-						>
-					</div>
-				</div>
-				<a href="#" class="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-500"
-					>Lost Password?</a
-				>
-			</div>
-			<button
-				type="submit"
-				class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>Login to your account</button
+<div class="py-6">
+	<div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+		<div
+			class="hidden lg:block lg:w-1/2 bg-cover"
+			style="background-image:url('https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80')"
+		/>
+		<div class="w-full p-8 lg:w-1/2">
+			<h2 class="text-2xl font-semibold text-gray-700 text-center">Brand</h2>
+			<p class="text-xl text-gray-600 text-center">Welcome back!</p>
+			<a
+				href="#"
+				class="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100"
 			>
-			<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-				Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500"
-					>Create account</a
+				<div class="px-4 py-3">
+					<svg class="h-6 w-6" viewBox="0 0 40 40">
+						<path
+							d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+							fill="#FFC107"
+						/>
+						<path
+							d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+							fill="#FF3D00"
+						/>
+						<path
+							d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+							fill="#4CAF50"
+						/>
+						<path
+							d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+							fill="#1976D2"
+						/>
+					</svg>
+				</div>
+				<h1 class="px-4 py-3 w-5/6 text-center text-gray-600 font-bold">Sign in with Google</h1>
+			</a>
+			<div class="mt-4 flex items-center justify-between">
+				<span class="border-b w-1/5 lg:w-1/4" />
+				<a href="#" class="text-xs text-center text-gray-500 uppercase">or login with email</a>
+				<span class="border-b w-1/5 lg:w-1/4" />
+			</div>
+			<div class="mt-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+				<input
+					class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+					type="email"
+				/>
+			</div>
+			<div class="mt-4">
+				<div class="flex justify-between">
+					<label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+					<a href="#" class="text-xs text-gray-500">Forget Password?</a>
+				</div>
+				<input
+					class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+					type="password"
+				/>
+			</div>
+			<div class="mt-8">
+				<button class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600"
+					>Login</button
 				>
 			</div>
-		</form>
+			<div class="mt-4 flex items-center justify-between">
+				<span class="border-b w-1/5 md:w-1/4" />
+				<a href="#" class="text-xs text-gray-500 uppercase">or sign up</a>
+				<span class="border-b w-1/5 md:w-1/4" />
+			</div>
+		</div>
 	</div>
-
-	<p class="mt-5">
-		This card component is part of a larger, open-source library of Tailwind CSS components. Learn
-		more by going to the official <a class="text-blue-600 hover:underline" href="#" target="_blank"
-			>Flowbite Documentation</a
-		>.
-	</p>
 </div>
